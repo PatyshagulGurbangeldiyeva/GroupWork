7e069a21c32c94b2dc33b76640cc85b960a89ce2
PatyshagulGurbangeldiyeva <patyshagulg@gmail.com>
2020-04-11 19:08:15 -0500

    first commit

---
+++ src/work/group/Task05.java
@@ -0,0 +1,12 @@
+package work.group;
+
+public class Task05 {
+
+	public static void main(String[] args) {
+		// Write a java program to reverse String? 
+		// Reverse a string word by word?
+
+		
+	}
+
+}
---
+++ src/work/group/o1IfCondition.java
@@ -0,0 +1,20 @@
+package work.group;
+
+public class o1IfCondition {
+
+	public static void main(String[] args) {
+		/*/* We have 7 days
+		 * if days are 2 or 3-----> SDLC class
+		 * if days are 6,7 -------> Java class
+		 * If days 1,5------------> Off day
+		 * if day 4---------------> Review class
+		 */
+		
+		 int day=2;
+		 if (day==2 && day==3) {
+			 System.out.println("SDLC class");
+		 }
+
+	}
+
+}
